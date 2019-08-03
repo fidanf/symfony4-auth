6689ee6e65228497e6e5bdcd39200e6295273147
fidanf <fidanza.frank@gmail.com>
2019-08-04 00:49:13 +0200

    Updated consequently to new .env changes in Symfony 4 (see: https://symfony.com/doc/current/configuration/dot-env-changes.html)

--- public/index.php
+++ public/index.php
@@ -7,12 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
-// The check is to ensure we don't use .env in production
+// The check is to ensure we don't use .env.local in production
 if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env.local file.');
     }
-    (new Dotenv())->load(__DIR__.'/../.env');
+    (new Dotenv())->load(__DIR__ . '/../.env.local');
 }
 
 if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
