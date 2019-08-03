fc86933c36a6357746a4524dbc4d173638d5dd78
fidanf <fidanza.frank@gmail.com>
2019-08-04 00:55:18 +0200

    Import Exception class

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
- 
+
+use Exception;
+
 class SecurityController extends AbstractController
 {
     /**
@@ -24,10 +26,10 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/logout", name="security_logout")
-     * @throws \Exception
+     * @throws Exception
      */
     public function logout(): void
     {
-        throw new \Exception('This should never be reached!');
+        throw new Exception('This should never be reached!');
     }
 }
