d6c32215bec08d1d68adac804d7fa53309f9baca
Frank Fidanza <fidanza.frank@gmail.com>
2020-09-10 14:48:13 +0000

    composer update, npm update

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,12 +4,12 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
